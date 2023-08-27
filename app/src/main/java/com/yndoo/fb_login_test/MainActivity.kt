cd7f9565cd0f49136c44a3b590ab65df3e365b5f
yndoo <yndoo2k@gmail.com>
2023-08-27 19:59:09 +0900

    파이어베이스 익명 로그인

---
+++ app/src/main/java/com/yndoo/fb_login_test/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.yndoo.fb_login_test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        auth = Firebase.auth
+
+        val btn = findViewById<Button>(R.id.noEmailLoginBtn)
+        btn.setOnClickListener {
+            auth.signInAnonymously()
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        val user = auth.currentUser
+                        Log.d("MainActivity", user!!.uid)
+
+                    } else {
+                        Toast.makeText(
+                            baseContext,
+                            "Authentication failed.",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                    }
+                }
+        }
+    }
+}
